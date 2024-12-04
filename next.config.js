6bcbeaf2102eb9e706a238264032c4547eefa1e2
Eric Munyi <irerieric@gmail.com>
2024-12-04 16:35:58 +0300

    chore: update next.config.js with necessary configurations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    domains: ['openweathermap.org'], // Allow loading images from openweathermap.org
+  },
 };
 
 module.exports = nextConfig;
